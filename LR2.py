bb049af6f0fbe26f43cb9f07705c8db4804b6c3b
rockentrav <kuzmenelena3@gmail.com>
2023-10-27 15:39:23 +0300

    addind program. task 3

---
+++ LR2.py
@@ -0,0 +1,22 @@
+import webbrowser
+import time
+
+webpages = [
+    "https://www.gosuslugi.ru",
+    "https://www.rzd.ru",
+    "https://www.rzd.ru",
+]
+
+interval = 10
+
+def open_webpages(webpages, interval):
+    for webpage in webpages:
+        print(f"Открываю страницу: {webpage}")
+        webbrowser.open(webpage)
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    open_webpages(webpages, interval)
+    
+
+
